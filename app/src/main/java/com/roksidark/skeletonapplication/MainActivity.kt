dc9ed3b52b0132fc65c641fecf3fd462bc6b4d02
roksidarkness <90980503+roksidarkness@users.noreply.github.com>
2024-05-08 22:39:58 +0200

    commit test 22

--- app/src/main/java/com/roksidark/skeletonapplication/MainActivity.kt
+++ app/src/main/java/com/roksidark/skeletonapplication/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android 11")
+                    Greeting("Android 22")
                 }
             }
         }
